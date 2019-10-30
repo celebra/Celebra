626d8afc6259ef00b1fd922295fcacd9155392ba
Anton Petrenko <57191172+celebra@users.noreply.github.com>
2019-10-30 20:38:46 +0200

    add Model: User

---
+++ Celebra.BL/Model/Gender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Celebra.BL.Model
+{
+    public class Gender
+    {
+        public string Name { get; }
+        public Gender(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ Celebra.BL/Model/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Celebra.BL.Model
+{
+    public class User
+    {
+        public string       Name        { get; }
+        public Gender       Gender      { get; }
+        public DateTime     BirthDate   { get; }
+        public double       Weight      { get; set; }
+        public double       Height      { get; set; }
+
+        public User(string name, Gender gender, DateTime birthDate, double weight, double height)
+        {
+            #region Check...
+            if (birthDate < DateTime.Parse("01.01.1900") || birthDate >= DateTime.Now)
+            {
+                throw new ArgumentNullException("Error Input Data", nameof(birthDate));
+            }
+            if (weight <= 0)
+            {
+                throw new ArgumentNullException("Error Input Data", nameof(weight));
+            }
+            if (height <= 0)
+            {
+                throw new ArgumentNullException("Error Input Data", nameof(height));
+            }
+            #endregion
+            Name        = name ?? throw new ArgumentNullException(nameof(name));
+            Gender      = gender ?? throw new ArgumentNullException(nameof(gender));
+            BirthDate   = birthDate;
+            Weight      = weight;
+            Height      = height;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
